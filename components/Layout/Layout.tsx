b42955eabc5b4c39c4fe9871869d3795099bc8a7
Nikos Kyknas <nkuknas@gmail.com>
2021-11-08 00:08:22 +0000

    make responsive for mobile

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 import { AdSense } from '../AdSense/AdSense';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
@@ -11,10 +12,10 @@ const MainContainer = styled.div`
   flex-direction: column;
 `;
 
-const BodyContainer = styled.div`
+const BodyContainer = styled.div<{ isMobile?: boolean }>`
   flex: 1 1 auto;
   display: flex;
-  flex-direction: row;
+  flex-direction: ${(props) => (props.isMobile ? 'column' : 'row')};
 `;
 
 const AdContainer = styled.div`
@@ -26,20 +27,45 @@ const ContentContainer = styled.div`
   flex: 2 1 auto;
 `;
 
-const Layout: React.FunctionComponent = ({ children }) => {
+const MainLayout: React.FunctionComponent = ({ children }) => {
   const { palette } = useTheme();
+  return (
+    <>
+      <AdContainer color={palette.background.paper}>
+        <AdSense />
+      </AdContainer>
+      <ContentContainer>{children}</ContentContainer>
+      <AdContainer color={palette.background.paper}>
+        <AdSense />
+      </AdContainer>
+    </>
+  );
+};
+
+const MobileLayout: React.FunctionComponent = ({ children }) => {
+  const { palette } = useTheme();
+  return (
+    <>
+      <ContentContainer>{children}</ContentContainer>
+      <AdContainer color={palette.background.paper}>
+        <AdSense />
+      </AdContainer>
+    </>
+  );
+};
+
+const Layout: React.FunctionComponent = ({ children }) => {
+  const { width } = useWindowDimensions();
+  const isMobile = width <= 768;
+  const Content = isMobile ? (
+    <MobileLayout>{children}</MobileLayout>
+  ) : (
+    <MainLayout>{children}</MainLayout>
+  );
   return (
     <MainContainer>
       <Header />
-      <BodyContainer>
-        <AdContainer color={palette.background.paper}>
-          <AdSense />
-        </AdContainer>
-        <ContentContainer>{children}</ContentContainer>
-        <AdContainer color={palette.background.paper}>
-          <AdSense />
-        </AdContainer>
-      </BodyContainer>
+      <BodyContainer isMobile={isMobile}>{Content}</BodyContainer>
       <Footer />
     </MainContainer>
   );
